93f0075cda9fb65ba1d2d2dfe0511d9dc3b902c5
ArulMani767 <arulmanik306@gmail.com>
2026-07-10 21:29:29 +0530

    added few programs

--- src/main/java/com/interview/ReverseStringSentenceWordByWordProgram.java
+++ src/main/java/com/interview/ReverseStringSentenceWordByWordProgram.java
@@ -4,33 +4,25 @@ import java.util.*;
 
 public class ReverseStringSentenceWordByWordProgram {
 
-    //inputs
-        // String inputString = "Java is a programming language";
-    public static String reverseTheSentence(String inputString) {
-        String[] words = inputString.split("\\s");
-
-        String outputString = "";
-
-        // UNCHANGED LOGIC: Reversing the word order
-        for (int i = words.length - 1; i >= 0; i--) {
-            outputString = outputString + words[i] + " ";
-        }
-
-        return outputString;
-    }
-
+    // inputs
+    // String inputString = "Java is a programming language";
     public static void main(String[] args) {
         // Try-with-resources cleanly manages the scanner instance automatically
         try (var sc = new Scanner(System.in)) {
 
             System.out.println("Enter Input String :");
-
             var inputString = sc.nextLine();
 
-            var outputString = reverseTheSentence(inputString);
+            // Split logic moved directly into main
+            String[] words = inputString.split("\\s");
+            String outputString = "";
+
+            // UNCHANGED LOGIC: Reversing the word order inside main method
+            for (int i = words.length - 1; i >= 0; i--) {
+                outputString = outputString + words[i] + " ";
+            }
 
             System.out.println("Input String : " + inputString);
-
             System.out.println("Output String : " + outputString);
         }
     }
